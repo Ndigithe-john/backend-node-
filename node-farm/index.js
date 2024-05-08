8f7b5927d1dfaacb3850ab317c5e37ba60c7b1b6
Ndigithe-john <ndigithejohn@gmail.com>
2024-05-08 15:43:44 +0300

    Reading and writting to file system in node js

---
+++ node-farm/index.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(textIn);
+const textOut = `This is what i know about avocado: ${textIn}. \n Created on ${Date.now()} `;
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log("file has been written ");
