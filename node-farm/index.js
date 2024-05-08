8654e89adaba769d6297b264f89f46783075a6c6
Ndigithe-john <ndigithejohn@gmail.com>
2024-05-08 16:15:03 +0300

    file system read and write

--- node-farm/index.js
+++ node-farm/index.js
@@ -4,3 +4,9 @@ console.log(textIn);
 const textOut = `This is what i know about avocado: ${textIn}. \n Created on ${Date.now()} `;
 fs.writeFileSync("./txt/output.txt", textOut);
 console.log("file has been written ");
+
+const textInAdd = fs.readFileSync("./txt/output.txt", "utf-8");
+console.log(textInAdd);
+const textOutAdd = `Avacado being the best fruit i have this to say: \n ${textInAdd} \n Lets see the number of lines output`;
+fs.writeFileSync("./txt/finalOut.txt", textOutAdd);
+console.log("Final Test");
