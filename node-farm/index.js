f82a579ae3f3ff7ebe83a7a4d4293cbc2387ac34
Ndigithe-john <ndigithejohn@gmail.com>
2024-05-09 14:05:47 +0300

    routing

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const http = require("http");
 
+const url = require("url");
+
 ///////////////////////////////////////
 //FILES
 // Blocking, synchronous way
@@ -50,7 +52,14 @@ const http = require("http");
 ////////////////////////////////////////////////////////
 //SERVER
 const server = http.createServer((req, res) => {
-  res.end("Hello from the server");
+  const pathName = req.url;
+  if (pathName === "/overview") {
+    res.end("This is the overview path");
+  } else if (pathName === "/product") {
+    res.end("This is the product path ");
+  } else {
+    res.end("Hello from the server");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
